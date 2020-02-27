87129dd627339acab79068bb2f3e3beafb821871
azizallouche <mohamedaziz.allouche@esprit.tn>
2020-02-27 14:26:12 -0800

    clavier setjouer

--- src/play.c
+++ src/play.c
@@ -5,14 +5,20 @@ void play(int *jouer)
  
     SDL_Surface *logoo = NULL;
     SDL_Rect poslogoo;
+   SDL_Event jj;
+ int a=1;
     poslogoo.x = 0;
     poslogoo.y = 0;
     logoo = IMG_Load("images/st.jpg");
     SDL_BlitSurface(logoo, NULL, screen, &poslogoo);
     while (*jouer == 1)
-    {
+    {  SDL_WaitEvent(&jj);
         SDL_BlitSurface(logoo, NULL, screen, &poslogoo);
-         setjouersouris(jouer);
+       if (jj.type == SDL_KEYDOWN)
+     {  if ( jj.key.keysym.sym == SDLK_ESCAPE ) 
+    {a++;}}
+   if (a % 2 == 0) 
+   {setjouersouris(jouer);}
         SDL_Flip(screen);
         
     }
--- src/setjouer.c
+++ src/setjouer.c
@@ -2,7 +2,7 @@
 
 SDL_Surface *setjouer = NULL;
 SDL_Rect possetjouer;
-int x3, y3, x4, y4;
+int  x3, y3,x4, y4,setj=-1;
 
 void setjouersouris(int *jouer)
 {
@@ -10,13 +10,29 @@ void setjouersouris(int *jouer)
     SDL_WaitEvent(&eventj);
     switch (eventj.type)
     {
-    case SDL_MOUSEMOTION:
-        x3 = eventj.motion.x;
-        y3 = eventj.motion.y;
-        break;
     case SDL_MOUSEBUTTONUP:
-        x4 = eventj.motion.x;
-        y4 = eventj.motion.y;
+        x4 = eventj.button.x;
+        y4 = eventj.button.y;
+        break;
+    case SDL_MOUSEBUTTONDOWN:
+        x3 = eventj.button.x;
+        y3 = eventj.button.y;
+        break;
+     case SDL_KEYDOWN:
+         {
+                   switch(eventj.key.keysym.sym)
+                   {
+                    case SDLK_UP:
+                      setj++;
+                     break;
+                    case SDLK_DOWN:
+                      setj++;
+                      break;
+                    case SDLK_RETURN:
+                     if (setj % 2 == 1){ *jouer = 0;}
+                    break;
+                  
+                   }}
         break;
     case SDL_QUIT:
         *jouer = 0;
@@ -28,20 +44,40 @@ void setjouersouris(int *jouer)
     setjouer = IMG_Load("images/setjouer0.png");
     SDL_BlitSurface(setjouer, NULL, screen, &possetjouer);
 
+   if (setj==-1)
+     {
+          setjouer=IMG_Load("images/setjouer0.png");
+          SDL_BlitSurface(setjouer,NULL,screen,&possetjouer);
+     }
+       
+   if (setj % 2 ==0)
+     {
+          setjouer=IMG_Load("images/setjouer1.png");
+          SDL_BlitSurface(setjouer,NULL,screen,&possetjouer);
+     }
+       
+   if (setj % 2 == 1)
+     {
+          setjouer=IMG_Load("images/setjouer2.png");
+          SDL_BlitSurface(setjouer,NULL,screen,&possetjouer);
+     }
     if ((x3 > 10) && (x3 < 10 + 64) && (y3 > 594) && (y3 < 594 + 68))
-    {
-        setjouer = IMG_Load("images/setjouer1.png");
-        SDL_BlitSurface(setjouer, NULL, screen, &possetjouer);
+    {    setj=0;
+        x3=0;
+        y3=0;
+        
     }
     if ((x3 > 10) && (x3 < 10 + 64) && (y3 > 682) && (y3 < 682 + 68))
-    {
-        setjouer = IMG_Load("images/setjouer2.png");
-        SDL_BlitSurface(setjouer, NULL, screen, &possetjouer);
+    {setj=1;
+       x3=0;
+        y3=0;
+       
     }
     if ((x4 > 10) && (x4 < 10 + 64) && (y4 > 682) && (y4 < 682 + 68))
-    {
-        *jouer = 0;
-        x4=0;
+    {*jouer = 0;
+       x4=0;
         y4=0;
+       
     }
+ SDL_Flip(screen);
 }
